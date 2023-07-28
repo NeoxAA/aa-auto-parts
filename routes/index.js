8b21c650ffe4e49cb7d2ec66c4a8c079471f70ca
Asam Afzal <58242302+NeoxAA@users.noreply.github.com>
2023-07-28 08:28:03 -0400

    more styling and added category list

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const upload = multer({ dest: './public/images/uploads/' });
 
 router.get("/", home_controller.index);
 
+router.get("/part/", function(req, res) {res.redirect(301, "/");});
 router.get("/part/create", parts_controller.part_create_get);
 router.post("/part/create", upload.single('image'), parts_controller.part_create_post);
 router.get("/part/:slug", parts_controller.part_detail);
@@ -16,6 +17,7 @@ router.post("/part/:slug/delete", parts_controller.part_delete_post);
 router.get("/part/:slug/update", parts_controller.part_update_get);
 router.post("/part/:slug/update", upload.single('image'), parts_controller.part_update_post);
 
+router.get("/category/", category_controller.category_list);
 router.get("/category/create", category_controller.category_create_get);
 router.post("/category/create", category_controller.category_create_post);
 router.get("/category/:slug", category_controller.category_detail);
